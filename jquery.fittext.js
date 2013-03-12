c8c3f3ebabc05a8aeca721e74bd75f2745dc8d10
Guins <contact@stephaneguigne.com>
2013-03-12 11:11:39 -0700

    add unFitText method for responsive purpose and update demo

--- jquery.fittext.js
+++ jquery.fittext.js
@@ -1,16 +1,23 @@
-/*global jQuery */
 /*!	
-* FitText.js 1.1
+* FitText.js 1.2
+*
+* UPDATE
+*   Desc    : Add unFitText method for responsive purposes
+*   Author  : Stéphane Guigné (http://stephaneguigne.com)
+*   Github  : https://github.com/guins/FitText.js
+*   Date    : Tue Mar 12 2013
+*   
 *
 * Copyright 2011, Dave Rupert http://daverupert.com
 * Released under the WTFPL license 
 * http://sam.zoy.org/wtfpl/
 *
-* Date: Thu May 05 14:23:00 2011 -0600
 */
 
 (function( $ ){
 	
+  var pluginName = 'FitTextJS';
+
   $.fn.fitText = function( kompressor, options ) {
 	   
     // Setup options
@@ -19,25 +26,52 @@
           'minFontSize' : Number.NEGATIVE_INFINITY,
           'maxFontSize' : Number.POSITIVE_INFINITY
         }, options);
-	
-    return this.each(function(){
 
-      // Store the object
-      var $this = $(this); 
+    return this.each(function()
+    {
+      var $this = $(this),
+          data = $this.data(pluginName);
+
+      if( !data )
+      {
+        var id = Math.random()*999999+100000>>0,
+            resizer = function () {
+              // Prevent differed window resize events
+              if( !$this.data(pluginName) ) return;
+
+              $this.css('font-size', Math.max(Math.min($this.width() / (compressor*10), parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize)));
+                console.log('sdcsd');
+            };
+
+        // Store Data
+        $this.data(pluginName, { id: id });
+
+        // Call once to set.
+        resizer();
         
-      // Resizer() resizes items based on the object width divided by the compressor * 10
-      var resizer = function () {
-        $this.css('font-size', Math.max(Math.min($this.width() / (compressor*10), parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize)));
-      };
-
-      // Call once to set.
-      resizer();
-				
-      // Call on resize. Opera debounces their resize by default. 
-      $(window).on('resize', resizer);
-      	
+        // Call on resize. Opera debounces their resize by default. 
+        $(window).on('resize.'+pluginName+'_'+id, resizer);
+      }
     });
 
+  };
+
+  $.fn.unfitText = function()
+  {
+    return this.each(function()
+    {
+      // Get stored data
+      var $this = $(this),
+          data = $this.data(pluginName);
+
+      if( data && data.id)
+      {
+        // Remove listenner
+        $(window).off('.'+pluginName+'_'+data.id);
+        // Reset styles and data
+        $this.css('font-size','').data(pluginName,null);
+      }
+    });
   };
 
 })( jQuery );
